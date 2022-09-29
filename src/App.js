15fd00f486d34e69e128d25dbb78688e499f3c23
Kamrul hasan <kamrulhasan275723@gmail.com>
2022-09-29 14:12:41 +0600

    ques and answer done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Items from './components/Items/Items';
+import Question from './components/Q and ans/Question';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Items></Items>
+      <Question></Question>
     </div>
   );
 }
