d7763134201119abb6216d2ac6ebb3eee2ea1486
Kamrul hasan <kamrulhasan275723@gmail.com>
2022-09-29 14:20:21 +0600

    footer done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Items from './components/Items/Items';
 import Question from './components/Q and ans/Question';
@@ -9,6 +10,7 @@ function App() {
       <Header></Header>
       <Items></Items>
       <Question></Question>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <h1>CopyRight @Kamrul Sakil 2022</h1>
+            <h2>Thank You</h2>
+        </div>
+    );
+};
+
+export default Footer;
